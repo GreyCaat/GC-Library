da8b9291bfa940617dc23ee368ccfa39f939df14
GreyCaat <66911792+GreyCaat@users.noreply.github.com>
2022-04-28 02:46:42 +0200

    Update Docu.lua

--- Docu.lua
+++ Docu.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreyCaat/GC-Library/main/Lib.txt", true))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreyCaat/GC-Library/main/Lib.txt", true))()
 --getting the library
 
 
